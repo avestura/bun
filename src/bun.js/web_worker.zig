9be289a7a20df35b63e3419031469ad71cb28658
Alistair Smith <hi@alistair.sh>
2025-05-30 15:25:33 -0700

    clean

--- src/bun.js/web_worker.zig
+++ src/bun.js/web_worker.zig
@@ -555,7 +555,6 @@ pub fn notifyNeedTermination(this: *WebWorker) callconv(.c) void {
 
     if (this.vm) |vm| {
         vm.eventLoop().wakeup();
-        // TODO(@190n) notifyNeedTermination
         vm.jsc.notifyNeedTermination();
     }
 
