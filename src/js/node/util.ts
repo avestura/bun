bebe83e99375658e1a92ad352b3fb82e04437dd7
Meghan Denny <meghan@bun.com>
2025-08-22 23:06:35 -0700

    Revert "tidy"
    This reverts commit 2d21bf0d75a3f6a55f8fe9f79d47846ad9f4f68d.

--- src/js/node/util.ts
+++ src/js/node/util.ts
@@ -236,9 +236,8 @@ var toUSVString = input => {
 
 function styleText(format, text) {
   validateString(text, "text");
-  if (!$isJSArray(format)) format = [format];
 
-  {
+  if ($isJSArray(format)) {
     let left = "";
     let right = "";
     for (const key of format) {
@@ -252,6 +251,10 @@ function styleText(format, text) {
     }
 
     return `${left}${text}${right}`;
+  }
+
+  if (format === "none") {
+    return text;
   }
 
   let formatCodes = inspect.colors[format];
